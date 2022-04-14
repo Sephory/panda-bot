8a663b459d21eca3a4540d6e75e311a04cfeba2d
Brian <brian@bteksoftware.com>
2022-04-14 10:50:06 -0700

    moved more config to env variables

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
+const channels = process.env.PANDA_CHANNELS.split(';')
+
 module.exports = {
-  options: { debug: true },
+  options: { debug: process.env.NODE_ENV === 'development' },
   identity: {
     username: process.env.PANDA_USERNAME,
     password: `oauth:${process.env.PANDA_OATH_TOKEN}`
   },
-  channels: ['sephory']
+  channels
 }
