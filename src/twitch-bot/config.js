da7216cbbdb7a525f68969dac5bc8c82682700e4
Brian <brian@bteksoftware.com>
2022-04-16 09:59:13 -0700

    moved twitch bot into it's own folder

--- src/twitch-bot/config.js
+++ src/twitch-bot/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const channels = process.env.PANDA_CHANNELS.split(';')
+const channels = process.env.PANDA_CHANNELS && process.env.PANDA_CHANNELS.split(';') || []
 
 module.exports = {
   options: { debug: process.env.NODE_ENV === 'development' },
